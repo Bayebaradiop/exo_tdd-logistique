f3968d5793581479c14fba20370e36fbbd3fcc57
Bayebaradiop <bayebara2000@gmail.com>
2026-07-24 19:35:41 +0000

    test: return two percent cashback during birthday month

--- src/test/java/sn/ecole221/cashback/CashbackRewardEngineTest.java
+++ src/test/java/sn/ecole221/cashback/CashbackRewardEngineTest.java
@@ -1,5 +1,7 @@
 package sn.ecole221.cashback;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +17,21 @@ class CashbackRewardEngineTest {
         int cashback = engine.calculate(transaction);
 
         assertEquals(100, cashback);
+    }
+
+    @Test
+    void shouldReturnTwoPercentCashbackDuringBirthdayMonth() {
+        Transaction transaction = new Transaction(
+                "USER-002",
+                10_000,
+                LocalDate.of(2026, 5, 15),
+                LocalDate.of(1990, 5, 3)
+        );
+
+        CashbackRewardEngine engine = new CashbackRewardEngine();
+
+        int cashback = engine.calculate(transaction);
+
+        assertEquals(200, cashback);
     }
 }
